63142109d9e1649de1ceae1ea89ee9b90ded620c
Daniel Stéphane <mbappedaniel00@yahoo.fr>
2023-12-17 20:19:41 +0100

    Add Card component, hover card, Fake data

---
+++ components/Card.vue
@@ -0,0 +1,139 @@
+<template>
+    <div class="mainContainer" @mouseenter="isHovering" @mouseleave="isNotHovering">
+        <img :src="background" :alt="`${subtitle} image`" />
+
+        <div class="container">
+            <p class="subtitle">
+                {{ subtitle }}
+            </p>
+    
+            <div class="mainContent">
+                <p class="title">
+                    {{ title }}
+                </p>
+    
+                <p class="description">
+                    {{ description }}
+                </p>
+            </div>
+
+            <div class="exploreButton">
+                <p class="exploreButtonText">
+                    {{ 'Explore More' }}
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    id: { type: Number, default: null },
+    subtitle: { type: String, default: '' },
+    title: { type: String, default: '' },
+    description: { type: String, default: '' },
+    background: { type: String, default: '' },
+})
+
+const hovering = ref(false)
+
+const isHovering = () => {
+    hovering.value = true;
+}
+
+const isNotHovering = () => {
+    hovering.value = false;
+}
+</script>
+
+<style lang="scss" scoped>
+.mainContainer {
+    border-radius: 8px;
+    overflow: hidden;
+    position: relative;
+    cursor: pointer;
+    background: gray;
+
+    .exploreButton {
+        padding: 12px;
+        margin-top: 50px;
+        border: 1px solid white;
+        border-radius: 8px;
+        opacity: 0;
+        transition: opacity 0.25s ease-in-out;
+    }
+}
+
+.mainContainer :hover {
+    .exploreButton {
+        opacity: 1;
+    }
+}
+
+.mainContainer :hover {
+    .container {
+        top: 50%;
+    }
+}
+
+.container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    position: absolute;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 18px;
+    top: 60%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+img {
+    width: 100%;
+    height: 100%;
+    opacity: 0.6;
+    background-size: cover;
+    background-position: center;
+}
+
+.subtitle {
+    text-transform: uppercase;
+    font-size: 12px;
+    font-weight: 400;
+    color: white;
+    text-align: center;
+    letter-spacing: 3px;
+}
+
+.mainContent {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    gap: 20px;
+}
+
+.title {
+    text-transform: capitalize;
+    font-size: 32px;
+    font-weight: 900;
+    color: white;
+    letter-spacing: 1px;
+}
+
+.description {
+    font-size: 14px;
+    max-width: 300px;
+    font-weight: 400;
+    color: white;
+    letter-spacing: 1px;
+}
+
+.exploreButtonText {
+    color: white;
+    font-weight: 700;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,133 @@
+<template>
+    <div class="container">
+        <Card
+            v-for="item in items"
+            :key="item?.id"
+            :subtitle="item?.subtitle"
+            :title="item?.title"
+            :description="item?.description"
+            :background="item?.background"
+        />
+    </div>
+</template>
+
+<script setup>
+import valtech from "../static/valtech.jpg"
+import venice from "../static/venice.jpg"
+import berlin from "../static/berlin.jpg"
+import barcelona from "../static/barcelona.jpg"
+import paris from "../static/paris.jpg"
+import amsterdam from "../static/amsterdam.jpg"
+import london from "../static/london.jpeg"
+
+const items = ref([
+    {
+        id: 0,
+        subtitle: 'Front-end',
+        title: 'valtech_',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        background: valtech,
+    },
+    {
+        id: 1,
+        subtitle: 'Italy',
+        title: 'Venice',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        background: venice,
+    },
+    {
+        id: 0,
+        subtitle: 'Germany',
+        title: 'Berlin',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        background: berlin,
+    },
+    {
+        id: 0,
+        subtitle: 'Spain',
+        title: 'Barcelona',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        background: barcelona,
+    },
+    {
+        id: 0,
+        subtitle: 'France',
+        title: 'Paris',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        background: paris,
+    },
+    {
+        id: 0,
+        subtitle: 'Netherlands',
+        title: 'Amsterdam',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        background: amsterdam,
+    },
+    {
+        id: 0,
+        subtitle: 'United Kingdom',
+        title: 'London',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        background: london,
+    },
+])
+</script>
+
+<style lang="scss" scoped>
+.container {
+    display: grid;
+    flex-direction: column;
+    padding: 12px;
+    gap: 12px;
+}
+
+@media only screen and (min-width: 768px) {
+    .container {
+        display: grid;
+        gap: 0px;
+        row-gap: 12px;
+        column-gap: 12px;
+    }
+    .container :nth-child(1) {
+        grid-column: 1 / span 3;
+        grid-row: 1 / span 2;
+    }
+}
+
+@media only screen and (min-width: 1280px) {
+    .container {
+        display: grid;
+        gap: 0px;
+        row-gap: 12px;
+        column-gap: 12px;
+    }
+    .container :nth-child(1) {
+        grid-column: 1 / span 4;
+        grid-row: 1 / span 2;
+    }
+    .container :nth-child(2) {
+        grid-column: 1 / span 2;
+        grid-row: 3 / span 1;
+    }
+    .container :nth-child(3) {
+        grid-column: 3 / span 1;
+        grid-row: 3 / span 1;
+    }
+    .container :nth-child(4) {
+        grid-column: 4 / span 1;
+        grid-row: 3 / span 1;
+    }
+    .container :nth-child(5) {
+        grid-column: 1 / span 1;
+        grid-row: 4 / span 1;
+    }
+    .container :nth-child(6) {
+        grid-column: 2 / span 1;
+        grid-row: 4 / span 1;
+    }
+    .container :nth-child(7) {
+        grid-column: 3 / span 2;
+        grid-row: 4 / span 1;
+    }
+}
+</style>
